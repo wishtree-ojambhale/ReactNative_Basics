d5f54251952512d4904943323542116439ead4da
wishtree-ojambhale <omkar.jambhale@wishtreetech.com>
2022-05-17 15:05:40 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,112 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
-import React from 'react';
-import type {Node} from 'react';
+
+import React,{useState,useEffect} from 'react';
+
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
-  View,
+  View,Button
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const App= () => {
+ 
+const [count, setCount] = useState(0);
+const [data, setData] = useState("");
+const [flag, setFlag] = useState(false);
+const [flag1, setFlag1] = useState(false);
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+useEffect(()=>{
+  count > 0 ? setFlag1(false):setFlag1(true)
+  if(count >= 10){
+    setData("Count limit reached")
+    setFlag(true)
+  }
+  if(count < 10){
+    setFlag(false)
+  }
+  
+})
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={css.body}>
+      <Text style={css.text}> State Management</Text>
+      <Text style={css.counter}>{count}</Text>
+     <View style={{flexDirection:'row',justifyContent:'space-evenly'}}>
+       <Button disabled={flag} onPress={()=>setCount(count+1)} title='Increment'/>
+      {count <10 ? <></>: <Text>{alert(data)}</Text> }  
+      <Button disabled={flag1} onPress={()=>setCount(count-1)} title='Decrement'/>
+      </View>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+
+const css = StyleSheet.create({
+
+  text : {
+    fontSize:40,alignSelf:'center',
+    color:'black'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  body:{
+    margin:10,
+    borderWidth:2,
+    flex:1,
+    padding:5
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  counter:{
+    color:'red',fontSize:40,alignSelf:'center',
   },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  
+})
+
 
 export default App;
+
+
+
+
+
+
+
