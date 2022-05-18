fb7437757d8f29cf58ddcf41de1c65e2fe919fac
wishtree-ojambhale <omkar.jambhale@wishtreetech.com>
2022-05-18 11:29:24 +0530

    Files formatted

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from './Login';
-import Signup from './Signup';
-import HomeScreen from './HomeScreen';
+import Login from './lib/Login';
+import Signup from './lib/Signup';
+import HomeScreen from './lib/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
